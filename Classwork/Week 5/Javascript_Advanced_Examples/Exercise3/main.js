f02d2e05e1e4024181d60aee1c9fb5d681ff82a3
Dom <reader.dominic@gmail.com>
2017-12-03 21:17:50 +0000

    Commits?

--- Classwork/Week 5/Javascript_Advanced_Examples/Exercise3/main.js
+++ Classwork/Week 5/Javascript_Advanced_Examples/Exercise3/main.js
@@ -1,4 +1,5 @@
-var roomObject = {
+var roomObject =
+{
 	title:'room name',
 	text:'you are standing in a room, there is a picture on the wall and a table',
 	choices:['examine picture','examine table']
@@ -7,7 +8,13 @@ var roomObject = {
 
 function OnLoad()
 {
-	
+	var title = <h3> "roomObject.title" </h3>;
+	document.getElementById("roomtitle").innerHTML("title");
+
+	var text = <p> "roomObject.text" </p>;
+  document.getElementById("roomtext").innerHTML("text");
+
+	var choies = <div> "roomObject.choices" </div>;
+	document.getElementById("roomchoices").innerHTML("choices");
 }
-
-
+}
