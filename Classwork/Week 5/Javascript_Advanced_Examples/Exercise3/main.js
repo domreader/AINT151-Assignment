2856aaf75be80f79b7c4717a5d7051f48776bf77
Dom <reader.dominic@gmail.com>
2017-12-03 21:25:38 +0000

    Updated exercise 3 week 5

--- Classwork/Week 5/Javascript_Advanced_Examples/Exercise3/main.js
+++ Classwork/Week 5/Javascript_Advanced_Examples/Exercise3/main.js
@@ -7,14 +7,20 @@ var roomObject =
 
 
 function OnLoad()
-{
-	var title = <h3> "roomObject.title" </h3>;
-	document.getElementById("roomtitle").innerHTML("title");
+	{
+		document.getElementById('roomtitle').innerHTML = roomObject.title
 
-	var text = <p> "roomObject.text" </p>;
-  document.getElementById("roomtext").innerHTML("text");
+		document.getElementById('roomtext').innerHTML = roomObject.text
+	}
 
-	var choies = <div> "roomObject.choices" </div>;
-	document.getElementById("roomchoices").innerHTML("choices");
-}
+	for(var i = 0; i < roomObject.choices.length; i++)
+	{
+		var tag = "<button type=\"button\">" + roomObject.choices[1] + "</button>";
+		document.getElementById('roomchoices').innerHTML += tag;
+
+	}
+
+
+
+
 }
