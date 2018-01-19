59931c506a49b78be0705d0cc46c4dff9c8268f8
Dom <reader.dominic@gmail.com>
2018-01-19 02:10:56 +0000

    Going over exercises

--- Classwork/Week 4- Javascript/Javascript_Examples/Javascript_Examples/Exercise4/Exercise4Javascript.js
+++ Classwork/Week 4- Javascript/Javascript_Examples/Javascript_Examples/Exercise4/Exercise4Javascript.js
@@ -3,3 +3,30 @@ function CreatePlayer()
 var Name = document.getElementById("playername").value;
 document.getElementById("name").innerHTML = Name;
 }
+
+{
+var color= document.getElementById("playerColour").value;
+document.getElementById('playerColour').innerHTML = color;
+}
+
+{
+var health = document.getElementById("playerhealth").value;
+document.getElementById('playerhealth').innerHTML = health;
+}
+
+{
+
+var weapon = document.getElementById("playerweapon").value;
+document.getElementById("playerweapon").innerHTML = weapon;
+
+if( weapon value == 1 ) {
+    Output “Crossbow of much hurting”
+}
+else if( weapon value == 2 ) {
+    Output “Broadsword of so slicing”
+}
+else {
+    Output “Wand of amaze magics”
+}
+
+}
